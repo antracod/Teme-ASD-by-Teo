3691fa98ef6f76db64c6bfe823f7918bc86cd5f7
Radu Teodor <raduteodorgeorge0@gmail.com>
2018-11-28 02:42:46 +0200

    Fix

--- Tema 9/1.cpp
+++ Tema 9/1.cpp
@@ -11,7 +11,6 @@ struct node
 	struct node *left, *right; 
 }; 
 
- 
 struct node *newNode(int item) 
 { 
 	struct node *temp = (struct node *)malloc(sizeof(struct node)); 
@@ -20,53 +19,35 @@ struct node *newNode(int item)
 	return temp; 
 } 
 
-/* A utility function to inserare a new node with given key in BST */
 struct node* inserare(struct node* node, int key) 
 { 
-	/* If the tree is empty, return a new node */
-	if (node == NULL) return newNode(key); 
-
-	/* Otherwise, recur down the tree */
+	if (node == NULL) 
+        return newNode(key); 
 	if (key < node->key) 
 		node->left = inserare(node->left, key); 
 	else if (key > node->key) 
 		node->right = inserare(node->right, key); 
-
-	/* return the (unchanged) node pointer */
 	return node; 
 } 
 
 struct node * minValueNode(struct node* node) 
 { 
     struct node* current = node; 
-  
-    /* loop down to find the leftmost leaf */
     while (current->left != NULL) 
         current = current->left; 
-  
     return current; 
 } 
 
 struct node* deleteNode(struct node* root, int key) 
 { 
-    // base case 
-    if (root == NULL) return root; 
-  
-    // If the key to be deleted is smaller than the root's key, 
-    // then it lies in left subtree 
+    if (root == NULL)
+     return root; 
     if (key < root->key) 
         root->left = deleteNode(root->left, key); 
-  
-    // If the key to be deleted is greater than the root's key, 
-    // then it lies in right subtree 
     else if (key > root->key) 
         root->right = deleteNode(root->right, key); 
-  
-    // if key is same as root's key, then This is the node 
-    // to be deleted 
     else
     { 
-        // node with only one child or no child 
         if (root->left == NULL) 
         { 
             struct node *temp = root->right; 
@@ -79,20 +60,12 @@ struct node* deleteNode(struct node* root, int key)
             free(root); 
             return temp; 
         } 
-  
-        // node with two children: Get the inorder successor (smallest 
-        // in the right subtree) 
         struct node* temp = minValueNode(root->right); 
-  
-        // Copy the inorder successor's content to this node 
         root->key = temp->key; 
-  
-        // Delete the inorder successor 
         root->right = deleteNode(root->right, temp->key); 
     } 
     return root; 
 }   
-
 
 void afis(struct node *root) 
 { 
@@ -104,19 +77,14 @@ void afis(struct node *root)
 	} 
 }
 
-// C function to search a given key in a given BST 
 struct node* cauta(struct node* root, int key) 
 { 
-	// Base Cases: root is null or key is present at root 
 	if (root == NULL || root->key == key) 
-	return root; 
-	
-	// Key is greater than root's key 
+	    return root; 
 	if (root->key < key) 
-	return cauta(root->right, key); 
-
-	// Key is smaller than root's key 
-	return cauta(root->left, key); 
+	    return cauta(root->right, key); 
+    else
+	    return cauta(root->left, key); 
 } 
 
 int gaseste_max(struct node* root) 
@@ -128,7 +96,6 @@ int gaseste_max(struct node* root)
 }
 
 
-// Driver Program to test above functions 
 int main() 
 { 
 	struct node *root = NULL; 
